bfc780c6853f79965f0fc4cc7c7ae48aa4a8242c
Subhasmita Sahu <158992092+subhascodes@users.noreply.github.com>
2026-04-09 00:07:59 +0530

    Remove credentials from test_connection.py
    Removed hardcoded username and password for security.

--- test_connection.py
+++ test_connection.py
@@ -1,8 +1,8 @@
 from neo4j import GraphDatabase
 
 URI = "neo4j://127.0.0.1:7687"
-USERNAME = "neo4j"
-PASSWORD = "namasteindia"
+USERNAME = ""
+PASSWORD = ""
 
 driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
 
